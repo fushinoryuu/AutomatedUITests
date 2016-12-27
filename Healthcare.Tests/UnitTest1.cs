4822c733687f9803f6d786f7a0f178d19d3ad4b0
Christian Munoz <Christian Munoz>
2016-12-26 22:35:12 -0700

    Converted from Nuget to Paket

--- Healthcare.Tests/UnitTest1.cs
+++ Healthcare.Tests/UnitTest1.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace Healthcare.Tests
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+        
         public void TestMethod1()
         {
         }
