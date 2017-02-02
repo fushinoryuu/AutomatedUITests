89c89b2c20a2e47b437b9fa2523e8ff0e3e0edc3
chrimuno <Christian.Munoz@extendhealth.com>
2017-02-02 07:03:59 -0700

    Added a few methods that can be very useful.

--- Healthcare.Framework/Pages/BasePage.cs
+++ Healthcare.Framework/Pages/BasePage.cs
@@ -1,4 +1,7 @@
-﻿using OpenQA.Selenium;
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Support.PageObjects;
 
@@ -13,5 +16,152 @@ namespace Healthcare.Framework.Pages
         protected WebDriverWait Wait;
 
         public abstract bool IsAt();
+
+        public virtual void WaitForPageToLoad()
+        {
+            WaitForPageToLoad(500);
+        }
+
+        protected void WaitForPageToLoad(int millisecondsSleep)
+        {
+            Wait.Until(driver => ((IJavaScriptExecutor)Driver).ExecuteScript("return document.readyState").Equals("complete"));
+            Thread.Sleep(millisecondsSleep);
+
+            WaitForAjax();
+            Thread.Sleep(millisecondsSleep);
+        }
+
+        // WaitForAjax method should be called as a last resort. Try to use explicit waits for elements instead
+        protected void WaitForAjax(int secondsTimeout = 5)
+        {
+            var isjQueryDefined = (bool)((IJavaScriptExecutor)Driver).ExecuteScript("return (typeof jQuery != 'undefined')");
+
+            if (isjQueryDefined)
+            {
+                new WebDriverWait(Driver, TimeSpan.FromSeconds(secondsTimeout)).Until(
+                    driver => ((IJavaScriptExecutor)driver).ExecuteScript("return jQuery.active == 0"));
+
+            }
+        }
+
+        public virtual void WaitUntilDisplayed(IWebElement element, int timeoutSeconds = 30)
+        {
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(timeoutSeconds));
+            wait.IgnoreExceptionTypes(typeof(NoSuchElementException), typeof(StaleElementReferenceException), typeof(TargetInvocationException));
+            wait.Message = "Element was not displayed within timeout of " + timeoutSeconds + " seconds";
+
+            wait.Until(d =>
+            {
+                //If the NoSuchElementException isn't caught here, VS will annoy you while debugging by stopping execution and
+                //letting you know it was thrown, even though it's handled in WebDriverWait.
+                try
+                {
+                    return element.Displayed;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (TargetInvocationException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public virtual void WaitUntilDisplayed(By by, int timeoutSeconds = 30)
+        {
+            new WebDriverWait(Driver, TimeSpan.FromSeconds(timeoutSeconds))
+            {
+                Message = "Could not find element with By " + by + " within timeout of " + timeoutSeconds + " seconds"
+            }.Until(ExpectedConditions.ElementIsVisible(by));
+        }
+
+        public virtual void WaitUntilExists(By by, int timeoutSeconds = 30)
+        {
+            new WebDriverWait(Driver, TimeSpan.FromSeconds(timeoutSeconds)).Until(ExpectedConditions.ElementExists(by));
+        }
+
+        public virtual void WaitUntilExists(IWebElement element, int timeoutSeconds = 30)
+        {
+            new WebDriverWait(Driver, TimeSpan.FromSeconds(timeoutSeconds))
+            {
+                Message = "Element was not displayed within timeout of " + timeoutSeconds + " seconds"
+            }.Until(d =>
+            {
+                try
+                {
+                    var accessElement = "element " + element + " can be accessed, so it exists";
+                    return accessElement.Length > 0;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public virtual bool ElementExists(IWebElement element)
+        {
+            try
+            {
+                //Just need to try to access the element
+                if (element.Displayed)
+                {
+                    return true;
+                }
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public virtual void WaitUntilDisplayedAndEnabled(IWebElement element, int timeoutSeconds = 30)
+        {
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(timeoutSeconds));
+            wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+
+            //unfortunately, the element can't be attached to the error message, because it might not exist yet and would throw NoSuchElementException before waiting for it
+            wait.Message = "Element was not displayed and enabled within timeout of " + timeoutSeconds + " seconds";
+
+            wait.Until(ExpectedConditions.ElementToBeClickable(element));
+        }
+
+        public virtual void WaitUntilNotExists(IWebElement element, int timeoutSeconds = 30)
+        {
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(timeoutSeconds));
+            wait.IgnoreExceptionTypes(typeof(NoSuchElementException), typeof(StaleElementReferenceException), typeof(TargetInvocationException));
+            wait.Message = "Element did not disappear within timeout of " + timeoutSeconds + " seconds";
+
+            wait.Until(d =>
+            {
+                //If the NoSuchElementException isn't caught here, VS will annoy you while debugging by stopping execution and
+                //letting you know it was thrown, even though it's handled in WebDriverWait. :(
+                try
+                {
+                    return !element.Displayed;
+                }
+                catch (Exception ex)
+                {
+                    if (ex is NoSuchElementException)
+                    {
+                        return true;
+                    }
+                    if (ex is TargetInvocationException || ex is StaleElementReferenceException)
+                    {
+                        return false;
+                    }
+
+                    //Should never get here, if we get here than the wait will swallow the stack trace :(
+                    throw;
+                }
+            });
+        }
     }
 }
---
+++ Healthcare.Framework/Utils/Extensions.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+
+namespace Healthcare.Framework.Utils
+{
+    public static class Extensions
+    {
+        public static void WaitForAttributeToVanish(this IWebElement element, string attributeName, string vanishingValue)
+        {
+            string attribute;
+            try
+            {
+                attribute = element.GetAttribute(attributeName);
+                if (attribute == null) return;
+            }
+            catch (Exception)
+            {
+                return;
+            }
+            var count = 0;
+            while (attribute.ToLower().Contains(vanishingValue) && count < 24)
+            {
+                try
+                {
+                    attribute = element.GetAttribute(attributeName);
+                }
+                catch (Exception)
+                {
+                    return;
+                }
+                count++;
+                Thread.Sleep(250);
+            }
+        }
+
+        public static void WaitForAttributeToAppear(this IWebElement element, string attributeName, string appearingValue)
+        {
+            var attribute = element.GetAttribute(attributeName);
+            var count = 0;
+            while (!attribute.ToLower().Contains(appearingValue) && count < 24)
+            {
+                attribute = element.GetAttribute(attributeName);
+                count++;
+                Thread.Sleep(250);
+            }
+        }
+
+        public static void WaitForStyleAttributeToStopChanging(this IWebElement element, string styleAttribute)
+        {
+            //split style string into attributes and values then locate the desired attribute.
+            var itemAttributes = element.GetAttribute("style").Split(new[] { ':', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            var attribute = string.Empty;
+            var attributeIndex = 1;
+            for (; attributeIndex < itemAttributes.Length; attributeIndex++)
+            {
+                if (!itemAttributes[attributeIndex - 1].Contains(styleAttribute)) continue;
+                attribute = itemAttributes[attributeIndex];
+                break;
+            }
+            //wait for value of attribute to maintain the same value.
+            var count = 0;
+            while (element.GetAttribute("style").Split(new[] { ':', ';' }, StringSplitOptions.RemoveEmptyEntries)[attributeIndex] != attribute && count < 20)
+            {
+                attribute =
+                    element.GetAttribute("style").Split(new[] { ':', ';' }, StringSplitOptions.RemoveEmptyEntries)[attributeIndex];
+                Debug.Print(attribute);
+                Thread.Sleep(100);
+                count++;
+            }
+        }
+
+        public static void WaitUntilDisplayed(this IWebElement element)
+        {
+            Thread.Sleep(2500);
+
+            var count = 0;
+            while (!element.Displayed && count < 40)
+            {
+                Thread.Sleep(100);
+                count++;
+            }
+        }
+
+        public static void EnterText(this IWebElement element, string text)
+        {
+
+            element.Click();
+            element.Clear();
+            element.SendKeys(text);
+
+            element.Click();
+        }
+
+        public static void Select(this IWebElement element, string text, string ifNullSelect = null)
+        {
+            if (text == null) text = ifNullSelect;
+            var select = new SelectElement(element);
+            select.SelectByText(text);
+        }
+
+        public static void Select(this IWebElement element, Enum givenEnum, string ifNullSelect = null)
+        {
+            if (givenEnum == null)
+                new SelectElement(element).SelectByText(ifNullSelect);
+
+            new SelectElement(element).SelectByEnum(givenEnum);
+        }
+
+        public static void Select(this IWebElement element, int index)
+        {
+            new SelectElement(element).SelectByIndex(index);
+        }
+
+        public static SelectElement ComboBox(this IWebElement element)
+        {
+            return new SelectElement(element);
+        }
+
+        public static void SelectByEnum(this SelectElement selectElement, Enum givenEnum)
+        {
+            selectElement.SelectByText(givenEnum.ToString());
+        }
+
+        public static ReadOnlyCollection<IWebElement> ContainAttribute(this ReadOnlyCollection<IWebElement> collection, string attributeName, string value)
+        {
+            var list = collection.Where(element => element.GetAttribute(attributeName).ToLower().Contains(value.ToLower())).ToList();
+            return new ReadOnlyCollection<IWebElement>(list);
+        }
+        
+        public static ReadOnlyCollection<IWebElement> FindElementAndContainers(this RemoteWebDriver driver, params By[] searchMechanisms)
+        {
+            var elements = new List<IWebElement>(searchMechanisms.Length) { driver.FindElement(searchMechanisms[0]) };
+
+            for (var index = 1; index < searchMechanisms.Length; index++)
+            {
+                var searchMechanism = searchMechanisms[index];
+                elements.Add(elements[index - 1].FindElement(searchMechanism));
+            }
+            return new ReadOnlyCollection<IWebElement>(elements);
+        }
+
+        public static IWebElement FindElementByContainers(this RemoteWebDriver driver, params By[] searchMechanisms)
+        {
+            var elements = driver.FindElementAndContainers(searchMechanisms);
+            return elements[elements.Count - 1];
+        }
+
+        public static IWebElement FindElementByPartialAttribute(this IWebElement element, By by, string attributeName, string value)
+        {
+            return
+                element.FindElements(by).FirstOrDefault(
+                    e => e.GetAttribute(attributeName).ToLower().Contains(value));
+        }
+
+        public static IWebElement FindElementByPartialAttribute(this RemoteWebDriver driver, By by, string attributeName, string value)
+        {
+            return
+                driver.FindElements(by).FirstOrDefault(
+                    e => e.GetAttribute(attributeName).ToLower().Contains(value));
+        }
+
+        public static IWebElement RefindElementAfterDomRefresh(this RemoteWebDriver driver, IWebElement element, By by)
+        {
+            element.WaitForDomRefresh();
+            return driver.FindElement(by);
+        }
+
+        public static void WaitForDomRefresh(this IWebElement element)
+        {
+            var count = 0;
+            var refreshed = false;
+            while (!refreshed && count < 20)
+            {
+                try
+                {
+                    var displayed = element.Displayed;
+                }
+                catch (Exception)
+                {
+                    refreshed = true;
+                }
+                Thread.Sleep(10);
+                count++;
+            }
+        }
+    }
+}
