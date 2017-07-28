fad107fe2c8cfe8728f5bc8b796a44931b064908
chrimuno <Christian.Munoz@extendhealth.com>
2017-07-27 23:28:35 -0600

    Updated the gitignore and added a controller along with some views.

---
+++ src/Automation.Gui/Controllers/SettingsController.cs
@@ -0,0 +1,123 @@
+﻿using System.Net;
+using System.Linq;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Automation.Gui.Models;
+
+namespace Automation.Gui.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private testsettingsEntities db = new testsettingsEntities();
+
+        // GET: Settings
+        public ActionResult Index()
+        {
+            return View(db.settings.ToList());
+        }
+
+        // GET: Settings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            setting setting = db.settings.Find(id);
+            if (setting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(setting);
+        }
+
+        // GET: Settings/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Settings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,targetBrowser,operatingSystem,seleniumHubUri,screenshotFolder")] setting setting)
+        {
+            if (ModelState.IsValid)
+            {
+                db.settings.Add(setting);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(setting);
+        }
+
+        // GET: Settings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            setting setting = db.settings.Find(id);
+            if (setting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(setting);
+        }
+
+        // POST: Settings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,targetBrowser,operatingSystem,seleniumHubUri,screenshotFolder")] setting setting)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(setting).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(setting);
+        }
+
+        // GET: Settings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            setting setting = db.settings.Find(id);
+            if (setting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(setting);
+        }
+
+        // POST: Settings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            setting setting = db.settings.Find(id);
+            db.settings.Remove(setting);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
