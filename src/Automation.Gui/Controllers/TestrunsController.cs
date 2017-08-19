d2bf5490e0c75009b18e654b48aecaac07479215
chrimuno <Christian.Munoz@extendhealth.com>
2017-08-19 08:39:18 -0600

    Created a new controller and view for testruns.

---
+++ src/Automation.Gui/Controllers/TestrunsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Automation.Gui.Models;
+
+namespace Automation.Gui.Controllers
+{
+    public class TestrunsController : Controller
+    {
+        private readonly testsettingsEntities _db = new testsettingsEntities();
+
+        // GET: Testruns
+        public ActionResult Index()
+        {
+            return View(_db.testruns.ToList());
+        }
+    }
+}
