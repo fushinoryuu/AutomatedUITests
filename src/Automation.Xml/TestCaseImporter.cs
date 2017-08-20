a5e0bc18f4922d1a504d6330a66eb42996d9f5ca
chrimuno <Christian.Munoz@extendhealth.com>
2017-08-19 23:54:46 -0600

    Created a couple classes to help desirialize xml files that have the formate in the sample.xml to make importing test cases easier.

---
+++ src/Automation.Xml/TestCaseImporter.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Automation.Xml.Utils;
+using System.Xml.Serialization;
+
+namespace Automation.Xml
+{
+    public class TestCaseImporter
+    {
+        public TestCaseImporter()
+        {
+            var serializer = new XmlSerializer(typeof(TestsFromXml));
+            var reader = new StreamReader(@"C:\AutomatedUiTests\resources\sample.xml");
+            var tests = (TestsFromXml) serializer.Deserialize(reader);
+            reader.Close();
+        }
+    }
+}
---
+++ src/Automation.Xml/Utils/TestsFromXml.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace Automation.Xml.Utils
+{
+    [XmlRoot("tests")]
+    public class TestsFromXml
+    {
+        [XmlElement("test-suite")]
+        public List<TestSuite> Suites { get; set; }
+    }
+
+    public class TestSuite
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlElement("test-case")]
+        public List<TestCase> TestCases { get; set; }
+    }
+
+    public class TestCase
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlElement("step")]
+        public List<string> TestSteps { get; set; }
+    }
+}
