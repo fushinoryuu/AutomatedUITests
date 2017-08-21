36029e99a3ac5a7d36db1dca063ce8bbcde06f6d
chrimuno <Christian.Munoz@extendhealth.com>
2017-08-20 22:16:50 -0600

    Added a new Controller and a few basic gui elements to display the test suites.

---
+++ src/Automation.Gui/Controllers/TestsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Data;
+using System.Linq;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using Automation.Xml;
+using Automation.Gui.Models;
+
+namespace Automation.Gui.Controllers
+{
+    public class TestsController : Controller
+    {
+        private readonly testsettingsEntities _db = new testsettingsEntities();
+
+        // GET: Tests
+        public ActionResult Index()
+        {
+            return View(_db.testsuites.ToList());
+        }
+    }
+}
