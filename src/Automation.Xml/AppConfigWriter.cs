e403d34523bcfe74bfc8938aca90044a83fa82be
chrimuno <Christian.Munoz@extendhealth.com>
2017-11-09 15:00:47 -0700

    Updated the app config files.

--- src/Automation.Xml/AppConfigWriter.cs
+++ src/Automation.Xml/AppConfigWriter.cs
@@ -55,7 +55,6 @@ namespace Automation.Xml
             var text = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
                        "\n<configuration>" +
                        "\n\t<configSections>" +
-                       "\n\t\t<!-- For more information on Entity Framework configuration, visit http://go.microsoft.com/fwlink/?LinkID=237468 -->" +
                        "\n\t\t<section name=\"entityFramework\" type=\"System.Data.Entity.Internal.ConfigFile.EntityFrameworkSection, EntityFramework, Version=6.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089\" requirePermission=\"false\" />" +
                        "\n\t</configSections>" +
                        "\n\t<entityFramework>" +
@@ -66,13 +65,13 @@ namespace Automation.Xml
                        "\n\t\t</defaultConnectionFactory>" +
                        "\n\t\t<providers>" +
                        "\n\t\t\t<provider invariantName=\"System.Data.SqlClient\" type=\"System.Data.Entity.SqlServer.SqlProviderServices, EntityFramework.SqlServer\" />" +
-                       "\n\t\t\t<provider invariantName=\"MySql.Data.MySqlClient\" type=\"MySql.Data.MySqlClient.MySqlProviderServices, MySql.Data.Entity.EF6, Version=6.9.9.0, Culture=neutral, PublicKeyToken=c5687fc88969c44d\" />" +
+                       "\n\t\t\t<provider invariantName=\"MySql.Data.MySqlClient\" type=\"MySql.Data.MySqlClient.MySqlProviderServices, MySql.Data.Entity.EF6, Version=6.9.10.0, Culture=neutral, PublicKeyToken=c5687fc88969c44d\" />" +
                        "\n\t\t</providers>" +
                        "\n\t</entityFramework>" +
                        "\n\t<system.data>" +
                        "\n\t\t<DbProviderFactories>" +
                        "\n\t\t\t<remove invariant=\"MySql.Data.MySqlClient\" />" +
-                       "\n\t\t\t<add name=\"MySQL Data Provider\" invariant=\"MySql.Data.MySqlClient\" description=\".Net Framework Data Provider for MySQL\" type=\"MySql.Data.MySqlClient.MySqlClientFactory, MySql.Data, Version=6.9.9.0, Culture=neutral, PublicKeyToken=c5687fc88969c44d\" />" +
+                       "\n\t\t\t<add name=\"MySQL Data Provider\" invariant=\"MySql.Data.MySqlClient\" description=\".Net Framework Data Provider for MySQL\" type=\"MySql.Data.MySqlClient.MySqlClientFactory, MySql.Data, Version=6.9.10.0, Culture=neutral, PublicKeyToken=c5687fc88969c44d\" />" +
                        "\n\t\t</DbProviderFactories>" +
                        "\n\t</system.data>" +
                        "\n\t<connectionStrings>" +
