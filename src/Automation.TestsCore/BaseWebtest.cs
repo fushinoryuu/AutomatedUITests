b9abd78bb462508885b37c11327e92c5e98df3b8
Christian Munoz <fushinoryuu@gmail.com>
2017-11-12 08:44:28 -0700

    Started on converting the Tests project but forgot that I need the Framework project first.

---
+++ src/Automation.TestsCore/BaseWebtest.cs
@@ -0,0 +1,36 @@
+﻿using Automation.SeleniumCore.Utils;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace Automation.TestsCore
+{
+    [TestFixture, Parallelizable]
+    public abstract class BaseWebtest : TestContainer
+    {
+        protected IRunSelenium Runner;
+
+        [SetUp]
+        public void Setup()
+        {
+            Runner = GetContainerInstance<IRunSelenium>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            var testStatus = TestContext.CurrentContext.Result.Outcome.Status;
+
+            // ReSharper disable once SwitchStatementMissingSomeCases
+            switch (testStatus)
+            {
+                case TestStatus.Failed:
+                case TestStatus.Inconclusive:
+                case TestStatus.Warning:
+                    Runner.TakeAndSaveScreenshot(TestContext.CurrentContext.Test.Name);
+                    break;
+            }
+
+            Runner.Cleanup();
+        }
+    }
+}
