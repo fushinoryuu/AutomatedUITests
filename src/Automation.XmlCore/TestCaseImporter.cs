c5e1bbe6993c882ef6c6dff50cca19d0077d68eb
chrimuno <Christian.Munoz@extendhealth.com>
2017-11-15 13:53:58 -0700

    Created the new Xml project.

---
+++ src/Automation.XmlCore/TestCaseImporter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using Automation.XmlCore.Utils;
+
+namespace Automation.XmlCore
+{
+    public class TestCaseImporter
+    {
+        private TestsFromXml _testsFromXml;
+
+        public bool ImportXmlFile(string path)
+        {
+            DesirializeXml(path);
+
+            if (_testsFromXml == null)
+                return false;
+
+            SaveSuites();
+            SaveTests();
+
+            return true;
+        }
+
+        private void DesirializeXml(string path)
+        {
+            try
+            {
+                var reader = new StreamReader(path);
+                var serializer = new XmlSerializer(typeof(TestsFromXml));
+
+                _testsFromXml = (TestsFromXml) serializer.Deserialize(reader);
+                reader.Close();
+            }
+            catch (FileNotFoundException)
+            {
+                _testsFromXml = null;
+            }
+        }
+
+        private void SaveSuites()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SaveTests()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Automation.XmlCore/Utils/TestsFromXml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Automation.XmlCore.Utils
+{
+    [XmlRoot("tests")]
+    internal class TestsFromXml
+    {
+        [XmlElement("test")]
+        public IList<Tests> ListOfTests { get; set; }
+
+        internal class Tests
+        {
+            [XmlAttribute("testsuite")]
+            public string TestSuite { get; set; }
+
+            [XmlAttribute("testname")]
+            public string TestName { get; set; }
+
+            [XmlAttribute("testcasedescription")]
+            public string TestCaseDescription { get; set; }
+        }
+    }
+}
