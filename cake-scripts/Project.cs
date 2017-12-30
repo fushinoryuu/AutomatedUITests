d1230f212f0a775d8ecdc7155f73b2e269bf2075
Christian Munoz <fushinoryuu@gmail.com>
2017-12-29 20:32:16 -0700

    Made updates to the cake scripts and deleted things that didn't make sense to have anymore.

--- cake-scripts/Project.cs
+++ cake-scripts/Project.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 public class Project
 {
     public string Name { get; set; }
+    public string Location { get; set; }
     public IEnumerable<string> Folders { get; set; }
     public IEnumerable<string> Files { get; set; }
 
