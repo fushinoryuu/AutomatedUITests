cdd101fd5aa778cc8399527e233957529cdd00e2
Christian Munoz <fushinoryuu@gmail.com>
2018-01-01 10:17:19 -0700

    Finished the home page and fixed the Thumbnail react component.

--- src/NetCore/Automation.GuiCore/Scripts/Shared/Thumbnail.jsx
+++ src/NetCore/Automation.GuiCore/Scripts/Shared/Thumbnail.jsx
@@ -4,9 +4,9 @@ export default class Thumbnail extends React.Component {
     render() {
         return (
             <div className="col-xs-6 col-md-3">
-                <a href="#" className="thumbnail">
+                <a href={this.props.link} className="thumbnail">
                     <img src={this.props.location} alt={this.props.altText} />
-                    <div class="caption">
+                    <div className="caption">
                         <h3 align="center">{this.props.caption}</h3>
                     </div>
                 </a>
