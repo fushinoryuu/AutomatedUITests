7bc6263b7aab43179b92a2337e1ff79a0004be51
Christian Munoz <fushinoryuu@gmail.com>
2018-01-02 22:12:53 -0700

    Working on getting React Table to work in my project.

--- src/NetCore/Automation.GuiCore/Scripts/Settings/SettingsPage.jsx
+++ src/NetCore/Automation.GuiCore/Scripts/Settings/SettingsPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Jumbotron from "../Shared/Jumbotron.jsx";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 // Jumbotron variables
 const settingsMainText = "Automation Settings";
 const settingsSubText = "You can use this page to:";
-const strings = ["Add new test configurations.",
+const strings = [
+    "Add new test configurations.",
     "Edit existing test configurations.",
     "Set which configuration to use for unit tests.",
     "Run unit tests from the UI."];
@@ -15,10 +18,42 @@ const settingsListItems = strings.map((string) =>
     </li>
 );
 
+// Table temp data
+const data = [{
+    targetBrowser: 'Chrome',
+    operatingSystem: 'Any',
+    seleniumHubUri: 'http://localhost:4444/wd/hub',
+    screenshotFolder: 'C:\\UiTestScreenShots\\',
+    isActive: true
+}];
+
+// Table variables
+const columns = [{
+    Header: 'Target Browser',
+    accessor: 'targetBrowser'
+}, {
+    Header: 'Operating System',
+    accessor: 'operatingSystem'
+}, {
+    Header: 'Selenium Hub Uri',
+    accessor: 'seleniumHubUri'
+}, {
+    Header: 'Screenshot Folder',
+    accessor: 'screenshotFolder'
+}, {
+    Header: 'Is Active',
+    accessor: 'isActive'
+}];
+
 // Main component to render the whole page
 class SettingsPage extends React.Component {
     render() {
-        return <Jumbotron mainText={settingsMainText} subText={settingsSubText} items={settingsListItems} />;
+        return (
+            <div>
+                <Jumbotron mainText={settingsMainText} subText={settingsSubText} items={settingsListItems} />
+                <ReactTable data={data} columns={columns} />
+            </div>
+        );
     }
 }
 
