99b52b4f24a729bef83536abaf6ecd7a6feb9744
Christian Munoz <fushinoryuu@gmail.com>
2018-03-19 23:26:19 -0600

    Readded some methods to the db helpers class.

--- src/NetCore/Automation.DatabaseCore/DbHelpers.cs
+++ src/NetCore/Automation.DatabaseCore/DbHelpers.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace Automation.DatabaseCore
@@ -13,6 +14,28 @@ namespace Automation.DatabaseCore
             builder.UseSqlServer(connectionString);
 
             return new AutomationDatabaseContext(builder.Options);
+        }
+
+        public static void DeactivateAll(IConfiguration configuration)
+        {
+            var db = OpenDbConnection(configuration);
+            var activeItems = db.TestConfigurations.Where(item => item.IsActive == 1).ToList();
+
+            // Set all items in the list to 0
+            activeItems.ForEach(item => item.IsActive = 0);
+
+            db.SaveChanges();
+            db.Dispose();
+        }
+
+        public static int GetNextId(IConfiguration configuration)
+        {
+            var db = OpenDbConnection(configuration);
+            var allTestConfigs = db.TestConfigurations.OrderBy(item => item.ConfigId).ToList();
+
+            db.Dispose();
+
+            return !allTestConfigs.Any() ? 1 : allTestConfigs.Last().ConfigId++;
         }
     }
 }
