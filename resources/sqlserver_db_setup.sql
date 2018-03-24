fa62c28e785de35ba1ef0d79e8c95ac18ddb6dcf
chrimuno <Christian.Munoz@extendhealth.com>
2018-03-23 23:05:05 -0600

    Finished the sql server setup script.

--- resources/sqlserver_db_setup.sql
+++ resources/sqlserver_db_setup.sql
@@ -3,10 +3,10 @@
 CREATE DATABASE AutomationDatabase
 GO
 
--- Create 'TestConfiguration' table
-
 USE AutomationDatabase
 GO
+
+-- Create 'TestConfiguration' table
 
 CREATE TABLE dbo.TestConfiguration
 (
@@ -14,13 +14,56 @@ CREATE TABLE dbo.TestConfiguration
     TargetBrowser VARCHAR(50) NOT NULL,
     OperatingSystem VARCHAR(50) NOT NULL,
     SeleniumHubUri VARCHAR(100) NOT NULL,
+    ScreenshotFolder VARCHAR(100) NOT NULL,
     IsActive BIT NOT NULL
+);
+GO
+
+-- Create 'TestRun' table
+
+CREATE TABLE dbo.TestRun
+(
+    RunId VARCHAR(36) NOT NULL PRIMARY KEY,
+    TestCaseCount INT NOT NULL,
+    Result VARCHAR(12) NOT NULL,
+    TestsPassed INT NOT NULL,
+    TestsFailed INT NOT NULL,
+    TestsInconclusive INT NOT NULL,
+    TestsSkipped INT NOT NULL,
+    StartTime DATETIME NOT NULL,
+    EndTime DATETIME NOT NULL,
+    Duration FLOAT NOT NULL
+);
+GO
+
+-- Create 'TestSuite' table
+
+CREATE TABLE dbo.TestSuite
+(
+    SuiteId INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    SuiteName VARCHAR(100) NOT NULL
+);
+GO
+
+-- Create 'TestCase' table
+
+CREATE TABLE dbo.TestCase
+(
+    CaseId INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    CaseName VARCHAR(100) NOT NULL,
+    BelongsToSuite INT FOREIGN KEY REFERENCES TestSuite(SuiteId),
+    CaseDescription VARCHAR(500) NOT NULL
 );
 GO
 
 -- Populate 'TestConfiguration' table
 
 INSERT INTO dbo.TestConfiguration
-    (TargetBrowser, OperatingSystem, IsActive)
-VALUES('Chrome', 'Any', 1)
+    (TargetBrowser, OperatingSystem, SeleniumHubUri, ScreenshotFolder, IsActive)
+VALUES('Chrome', 'Any', 'http://localhost:4444/wd/hub', 'C:\\UiTestScreenshots\\', 1)
+GO
+
+-- Verify the data in the 'TestConfiguration' table
+
+SELECT * FROM dbo.TestConfiguration
 GO
